19b2085dde4a7b39eed12d6dc546f5d409555bf2
maniaru455 <maniaru455@gmail.com>
2022-11-18 14:31:19 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <User
+        name="Arjun"
+        pic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFacn0pb12vsSRTPKRUDnxFrP8sj7st13r8A&usqp=CAU" />
+      <User
+        name="Monisha"
+        pic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3WEmfJCME77ZGymWrlJkXRv5bWg9QQmQEzw&usqp=CAU" />
+      <User
+        name="Saravanan"
+        pic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi9F290eYA84VTdmPklY5zLD_ZLUiZsKLo0A&usqp=CAU" />
+
+
+    </div >
+  );
+}
+function User({ pic, name }) {
+  return (
+    <section>
+      <img className="user-profile-pic" src={pic} alt={name} />
+      <h1 className="user-name">
+        Hello,<span className="user-first-name">{name}</span>>
+      </h1>
+
+    </section>
+
   );
 }
 
 export default App;
+
+
+
